713250a6856aace2dab070161554e44a1435d3b7
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-04-29 08:44:54 +0000

    Removed temporary build files

--- geye.cpp
+++ geye.cpp
@@ -23,7 +23,7 @@ void testrun()
     if(!ge_cap.isOpened()) exit(1);
 
     // create haar classifier cascade
-    CascadeClassifier ge_cap_haar("c:\\opencv\\data\\haarcascades\\haarcascade_frontalface_alt2.xml");
+    CascadeClassifier ge_cap_haar("c:\\opencv\\data\\haarcascades\\haarcascade_frontalface_default.xml");
 
 
     // temporary variables for the loop
@@ -36,22 +36,22 @@ void testrun()
         ge_cap >> ge_cap_img;
 
         // do image processing stuff here
-        ge_cap_haar.detectMultiScale(ge_cap_img, faces, 1.1, 4, 0, Size(96,132));
+//        ge_cap_haar.detectMultiScale(ge_cap_img, faces, 1.1, 4, 0, Size(96,132));
 
         // draw rectangles of faces detected
-        count = faces.size();
-        for(i = 0; i < count; i++)
-        {
-            rectangle(ge_cap_img, faces[i].tl(), faces[i].br(), CV_RGB(0,255,0));
-        }
+//        count = faces.size();
+//        for(i = 0; i < count; i++)
+//        {
+//            rectangle(ge_cap_img, faces[i].tl(), faces[i].br(), CV_RGB(0,255,0));
+//        }
 
         // show the image stored in buffer
         imshow(WIN_NAME, ge_cap_img);
     }
 
-    while(waitKey(12) != 'q')
-    {
-
-    }
+//    while(waitKey(12) != 'q')
+//    {
+//
+//    }
 
 }
