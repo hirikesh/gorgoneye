51f54111b321f6763e000398897b340d7ff64b9b
malcoholic <malcoholic@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-07-22 03:24:49 +0000

    r19 description wrong due to unimelb proxy server issues. this update is actually: added GEyeDetector class as a generic detector, modified qt gui to include haar face detection

--- trunk/geyetracker.cpp
+++ trunk/geyetracker.cpp
@@ -9,18 +9,40 @@ GEyeTracker::GEyeTracker(QWidget *parent) :
 
     capture = VideoCapture(0);
     capture >> image;
+
+    ged = GEyeDetector(&image, 1.1, 4, Size(96,132));
+
     m_i = QImage(image.data,
                  image.size().width,
                  image.size().height,
                  QImage::Format_RGB888);
 
     m_timer = new QTimer(this);
-    m_timer->setInterval(30);
+    m_timer->setInterval(40);
 
     connect(m_timer, SIGNAL(timeout()), this, SLOT(procFrame()));
     connect(ui->startBtn, SIGNAL(clicked()), m_timer, SLOT(start()));
+    connect(ui->startBtn, SIGNAL(clicked()), this, SLOT(disableParams()));
     connect(ui->stopBtn, SIGNAL(clicked()), m_timer, SLOT(stop()));
-    ged = GEyeDetector(&image, 1.1, 4, Size(96,132));
+    connect(ui->stopBtn, SIGNAL(clicked()), this, SLOT(enableParams()));
+
+    connect(ui->heightSlider, SIGNAL(valueChanged(int)), ui->heightSpinBox, SLOT(setValue(int)));
+    connect(ui->heightSpinBox, SIGNAL(valueChanged(int)), ui->heightSlider, SLOT(setValue(int)));
+    connect(ui->widthSlider, SIGNAL(valueChanged(int)), ui->widthSpinBox, SLOT(setValue(int)));
+    connect(ui->widthSpinBox, SIGNAL(valueChanged(int)), ui->widthSlider, SLOT(setValue(int)));
+    connect(ui->minNSlider, SIGNAL(valueChanged(int)), ui->minNSpinBox, SLOT(setValue(int)));
+    connect(ui->minNSpinBox, SIGNAL(valueChanged(int)), ui->minNSlider, SLOT(setValue(int)));
+
+    connect(ui->heightSpinBox, SIGNAL(valueChanged(int)), this, SLOT(setHeight(int)));
+    connect(ui->widthSpinBox, SIGNAL(valueChanged(int)), this, SLOT(setWidth(int)));
+    connect(ui->minNSpinBox, SIGNAL(valueChanged(int)), this, SLOT(setMinN(int)));
+    connect(ui->scaleSpinBox, SIGNAL(valueChanged(double)), this, SLOT(setScale(double)));
+
+    ui->heightSpinBox->setValue(ged.size().height);
+    ui->widthSpinBox->setValue(ged.size().width);
+    ui->minNSpinBox->setValue(ged.minneighbours());
+    ui->scaleSpinBox->setValue(ged.scale());
+
 }
 
 GEyeTracker::~GEyeTracker()
@@ -32,8 +54,8 @@ void GEyeTracker::procFrame()
 {
     capture >> image;
     cvtColor(image, image, CV_BGR2RGB);
-    //Rect r = ged.detect();
-    //m_faceLoc = QRect(QPoint(r.x,r.y),QSize(r.width,r.height));
+    Rect r = ged.detect();
+    m_faceLoc = QRect(QPoint(r.x,r.y),QSize(r.width,r.height));
     this->update();
 }
 
@@ -51,4 +73,46 @@ void GEyeTracker::paintEvent(QPaintEvent* e)
                          m_faceLoc.width(),
                          m_faceLoc.height()));
     }
+}
+
+void GEyeTracker::setScale(double sf)
+{
+    ged.setScaleFactor(sf);
+}
+
+void GEyeTracker::setMinN(int mn)
+{
+    ged.setMinNeighbours(mn);
+}
+
+void GEyeTracker::setWidth(int w)
+{
+    ged.setMinSize(Size(w, ged.size().height));
+}
+
+void GEyeTracker::setHeight(int h)
+{
+    ged.setMinSize(Size(ged.size().width, h));
+}
+
+void GEyeTracker::disableParams()
+{
+    ui->scaleSpinBox->setEnabled(false);
+    ui->widthSlider->setEnabled(false);
+    ui->widthSpinBox->setEnabled(false);
+    ui->heightSlider->setEnabled(false);
+    ui->heightSpinBox->setEnabled(false);
+    ui->minNSlider->setEnabled(false);
+    ui->minNSpinBox->setEnabled(false);
+}
+
+void GEyeTracker::enableParams()
+{
+    ui->scaleSpinBox->setEnabled(true);
+    ui->widthSlider->setEnabled(true);
+    ui->widthSpinBox->setEnabled(true);
+    ui->heightSlider->setEnabled(true);
+    ui->heightSpinBox->setEnabled(true);
+    ui->minNSlider->setEnabled(true);
+    ui->minNSpinBox->setEnabled(true);
 }
