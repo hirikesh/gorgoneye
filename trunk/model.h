016dd41dc8a64471961b357cc84e2366d814666e
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-08-18 16:20:31 +0000

    Initial (broken) commit of video capturing code within Model, capturing from webcam and writing to Store's sceneImg.

--- trunk/model.h
+++ trunk/model.h
@@ -1,13 +1,22 @@
-#ifndef MODEL_H
-#define MODEL_H
+#include <cv.h>
+#include <highgui.h>
 #include "trackers/facetracker.h"
 #include "parameter.h"
+#include "store.h"
+
+#ifndef MODEL_H
+#define MODEL_H
+
 class Model
 {
 public:
-    Model(Store*);
+    Model(int device);
+    void update(); // instructs model to grab new frame and process it, thereby updating Store.
+    Store* getStore();
     vector<BaseDetector*> getTrackerParams(); // needs to be an array of this in final
 private:
+    Store store;
+    VideoCapture capture;
     FaceTracker faceTracker;
     // Eye eyeTracker, Nose noseTracker, etc.
     // or possibly an array of pointers to Trackers?
