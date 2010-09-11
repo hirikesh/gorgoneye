7092bf1b9576298e354f9dd7a785a70117053221
malcoholic <malcoholic@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-09-11 19:48:52 +0000

    >> we no longer preprocess the BGR webcam capture into RGB - this does not seem to affect haar performance. >> opengl texture generation of webcam capture has been updated to reflect the above change >> cleaned up openGL code; way, way too messy and many unnecessary commands being run

--- trunk/detectors/featuredetector.cpp
+++ trunk/detectors/featuredetector.cpp
@@ -30,7 +30,7 @@ bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
     static Mat maskImg(srcImg.rows, srcImg.cols, CV_8UC1);
 
     // Extract Hue Info
-    cvtColor(srcImg, cHSVImg, CV_BGR2HSV);
+    cvtColor(srcImg, cHSVImg, CV_RGB2HSV);
     Mat cHSVChannels[] = {hueImg, satImg, valImg};
     split(cHSVImg, cHSVChannels);
 
@@ -40,7 +40,8 @@ bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
     valImg = Scalar(255);
     Mat hueVis[] = {hueImg, satImg, valImg};
     merge(hueVis, 3, hueVisImg);
-    cvtColor(hueVisImg, hueVisImg, CV_HSV2RGB);
+    // This colour conversion is extremely laggy.
+//   cvtColor(hueVisImg, hueVisImg, CV_HSV2RGB);
 //    imshow("Hue Visualisation", hueVisImg);
 
     // set mask ROI
--- trunk/glview.cpp
+++ trunk/glview.cpp
@@ -23,45 +23,29 @@ QSize GLView::sizeHint() const
 
 void GLView::initializeGL()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // clears window to RGB 000 ie. black
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // set the window clear color to black
     glEnable(GL_TEXTURE_2D);
     glShadeModel(GL_FLAT);
-    glDisable(GL_DEPTH_TEST);    // Disables Depth Testing
-    glClear(GL_COLOR_BUFFER_BIT);
-
 }
 
 void GLView::resizeGL(int width, int height)
 {
-    if (height == 0)    // Avoid Divide-By-Zero
+    if (!height)    // Avoid Divide-By-Zero
     { height = 1;}
 
-    glViewport(0, 0, width, height);	// Reset The Current Viewport
-    glMatrixMode(GL_PROJECTION);	// Select The Projection Matrix
-    glLoadIdentity();			// Reset The Projection Matrix
+    glViewport(0, 0, (GLsizei) width, (GLsizei) height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
     gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);     // Select The Modelview Matrix
-    glLoadIdentity();               // Reset The Modelview Matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 void GLView::paintGL()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // clears window to RGB 000 ie. black
-    glEnable(GL_TEXTURE_2D);
-    glShadeModel(GL_FLAT);
-    glDisable(GL_DEPTH_TEST);    // Disables Depth Testing
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glViewport(0, 0, this->width(), this->height());	// Reset The Current Viewport
-    glMatrixMode(GL_PROJECTION);	// Select The Projection Matrix
-    glLoadIdentity();			// Reset The Projection Matrix
-    gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);     // Select The Modelview Matrix
-    glLoadIdentity();               // Reset The Modelview Matrix
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); // clears window
     glLoadIdentity();
-    glEnable2D();
     glBindTexture(GL_TEXTURE_2D, texture[0]);
     glBegin(GL_QUADS);  // Draw A Quad
     glTexCoord2f(0.0f, 0.0f); glVertex2f(0, 480); // Top Left
@@ -69,7 +53,6 @@ void GLView::paintGL()
     glTexCoord2f(1.0f, 1.0f); glVertex2f(640, 0); // Bottom Right
     glTexCoord2f(1.0f, 0.0f); glVertex2f(640, 480); // Top Right
     glEnd();
-    glDisable2D();
     update();
 }
 
@@ -79,31 +62,7 @@ void GLView::loadGLTextures(const Mat& image)
     glBindTexture( GL_TEXTURE_2D, texture[0] );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, image.data);
-}
-
-void GLView::glEnable2D()
-{
-    //int vPort[4];
-
-    //glGetIntegerv(GL_VIEWPORT, vPort);
-
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    //glOrtho(0, vPort[2], 0, vPort[3], -1, 1);
-    gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-}
-
-void GLView::glDisable2D()
-{
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, image.data);
 }
 
 void GLView::setCurrROI(QRect * r)
