23507ae7e0e17fb355422239625dcfa65fa21af6
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-09-30 14:20:00 +0000

    Smooth and downsample for haar detector as well.

--- trunk/detectors/haardetector.cpp
+++ trunk/detectors/haardetector.cpp
@@ -12,23 +12,21 @@ HaarDetector::HaarDetector(const int type, string td, double sf, int mn, bool fg
     minSize(ms)
 {
     // Create Parameters that will be used
-    params.push_back(new RangeParam<double>("Scale Factor", Param::RANGE_DBL, &scaleFactor, 1, 2, 0.05));
-    params.push_back(new RangeParam<int>("Min. Neighbours", Param::RANGE, &minNeighbours, 1, 5, 1));
-    params.push_back(new RangeParam<int>("Min. Search Width", Param::RANGE, &minSize.width, 20, 200, 20));
-    params.push_back(new RangeParam<int>("Min. Search Height", Param::RANGE, &minSize.height, 20, 200, 20));
+    params.push_back(new RangeParam<double>("Scale Factor", Param::RANGE_DBL, &scaleFactor, 1.05, 2, 0.05));
+    params.push_back(new RangeParam<int>("Min. Neighbours", Param::RANGE, &minNeighbours, 1, 7, 1));
+    params.push_back(new RangeParam<int>("Min. Search Width", Param::RANGE, &minSize.width, 8, 200, 20));
+    params.push_back(new RangeParam<int>("Min. Search Height", Param::RANGE, &minSize.height, 8, 200, 20));
     params.push_back(new ModeParam("Enable Canny Pruning", &flags, false));
 }
 
 bool HaarDetector::locate(const Mat& srcImg, Rect& srcRoi)
 {
-//    static Mat haarImg(srcImg.rows, srcImg.cols, CV_8UC3);
-//    cvtColor(srcImg, haarImg, CV_BGR2RGB);
+    static Mat haarImg(srcImg.rows, srcImg.cols, CV_8UC3);
     static CascadeClassifier cc(trainingData);
     static vector<Rect> rois;
 
-//    pyrDown(srcImg, haarImg);
-//    pyrDown(haarImg, haarImg);
-    cc.detectMultiScale(srcImg,
+    pyrDown(srcImg, haarImg);
+    cc.detectMultiScale(haarImg,
                         rois,
                         scaleFactor,
                         minNeighbours,
@@ -37,9 +35,9 @@ bool HaarDetector::locate(const Mat& srcImg, Rect& srcRoi)
 
     if(rois.size())
     {
-        srcRoi = rois[0];
+//        srcRoi = rois[0];
 //        srcRoi = (rois[0] + rois[0].size()) + rois[0].tl();
-//        srcRoi = Rect(rois[0].x*2, rois[0].y*2, rois[0].width*2, rois[0].height*2);
+        srcRoi = Rect(rois[0].x*2, rois[0].y*2, rois[0].width*2, rois[0].height*2);
         return true;
     }
     else
