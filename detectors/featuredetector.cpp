7bd5032547a0eab259e36a4e5b5778adb1441ed5
malcoholic <malcoholic@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-10-02 09:03:04 +0000

    >> added Cb and Cr viewing options

--- detectors/featuredetector.cpp
+++ detectors/featuredetector.cpp
@@ -1,22 +1,34 @@
 #include <cv.h>
+#include <highgui.h>
 #include "featuredetector.h"
 
 using namespace cv;
 
-FeatureDetector::FeatureDetector(const int type, int mins, int maxs, int minv, int maxv) :
+FeatureDetector::FeatureDetector(const int type, int mins, int maxs, int minv, int maxv,
+                                                 int mincb, int maxcb, int mincr, int maxcr) :
     BaseDetector(type, "CAMShift"),
     histCalibrate(true),
-    minSaturation(mins), // default
-    maxSaturation(maxs), // default
-    minValue(minv), // default
-    maxValue(maxv) // default
+    minSaturation(mins),
+    maxSaturation(maxs),
+    minValue(minv),
+    maxValue(maxv),
+    minChromaBlue(mincb),
+    maxChromaBlue(maxcb),
+    minChromaRed(mincr),
+    maxChromaRed(maxcr)
 {
     params.push_back(new RangeParam<int>("Min. Saturation", Param::RANGE, &minSaturation, 0, 255, 5));
     params.push_back(new RangeParam<int>("Max. Saturation", Param::RANGE, &maxSaturation, 0, 255, 5));
     params.push_back(new RangeParam<int>("Min. Value", Param::RANGE, &minValue, 0, 255, 5));
     params.push_back(new RangeParam<int>("Max. Value", Param::RANGE, &maxValue, 0, 255, 5));
+    params.push_back(new RangeParam<int>("Min. Cb", Param::RANGE, &minChromaBlue, 0, 255, 5));
+    params.push_back(new RangeParam<int>("Max. Cb", Param::RANGE, &maxChromaBlue, 0, 255, 5));
+    params.push_back(new RangeParam<int>("Min. Cr", Param::RANGE, &minChromaRed, 0, 255, 5));
+    params.push_back(new RangeParam<int>("Max. Cr", Param::RANGE, &maxChromaRed, 0, 255, 5));
     params.push_back(new ImageModeParam("Back Projected Image", &backProjGrayImg));
     params.push_back(new ImageModeParam("Hue Visualisation Image", &hueVisImg));
+    params.push_back(new ImageModeParam("Cb Component Image", &chromaBlueVisImg));
+    params.push_back(new ImageModeParam("Cr Component Image", &chromaRedVisImg));
 }
 
 bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
@@ -35,17 +47,45 @@ bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
 
     // visualise Hue for debugging
     static Mat emptyImg(srcImgSize, CV_8UC1, Scalar(255));
+    //static Mat emptyImg(srcImg.size(), CV_8UC1, Scalar(255));
+
     static Mat hueVis[] = {hueImg, emptyImg, emptyImg};
     merge(hueVis, 3, hueVisImg);
-    // This colour conversion is extremely laggy.
-    cvtColor(hueVisImg, hueVisImg, CV_HSV2RGB);
 
-    // set mask ROI
+    // This colour conversion is extremely laggy.
+    //cvtColor(hueVisImg, hueVisImg, CV_HSV2RGB);
+
+    static Mat neutralImg(srcImgSize, CV_8UC1, Scalar(128));
+    static Mat cYCbCrImg(srcImgSize, CV_8UC3);
+    static Mat lumaImg(srcImgSize, CV_8UC1);
+    static Mat chromaBlueImg(srcImgSize, CV_8UC1);
+    static Mat chromaRedImg(srcImgSize, CV_8UC1);
+    static Mat cYCbCrChannels[] = {lumaImg, chromaBlueImg, chromaRedImg};
+
+    static Mat chromaBlueVis[] = {neutralImg, chromaBlueImg, neutralImg};
+    merge(chromaBlueVis, 3, chromaBlueVisImg);
+    static Mat chromaRedVis[] = {neutralImg, neutralImg, chromaRedImg};
+    merge(chromaRedVis, 3, chromaRedVisImg);
+
+    cvtColor(srcImg, cYCbCrImg, CV_BGR2YCrCb);
+    split(cYCbCrImg, cYCbCrChannels);
+
+    // set Mask ROI (YCbCr)
+    static Mat maskChromaImg(srcImgSize, CV_8UC1);
+
+    inRange(cYCbCrImg,
+            Scalar(0, minChromaBlue, minChromaRed),
+            Scalar(255, maxChromaBlue, maxChromaRed),
+            maskChromaImg);
+
+    // set mask ROI (HSV)
     static Mat maskImg(srcImgSize, CV_8UC1);
+
     inRange(cHSVImg,
             Scalar(0, minSaturation, minValue),
             Scalar(180, maxSaturation, maxValue),
             maskImg);
+    bitwise_and(maskImg, maskChromaImg, maskImg, MatND());
     static Mat hueImgROI, maskROI;
     hueImgROI = hueImg(srcRoi);
     maskROI = maskImg(srcRoi);
@@ -91,6 +131,7 @@ bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
     // show back projection for debugging / parameter tweaking
     static Mat backProjImg3[] = {backProjImg, backProjImg, backProjImg};
     bitwise_and(backProjImg, maskImg, backProjImg, MatND());
+    //bitwise_and(backProjImg, maskChromaImg, backProjImg, MatND());
     merge(backProjImg3, 3, backProjGrayImg);
 
     // CAMShift Calculations ---------
@@ -109,7 +150,7 @@ bool FeatureDetector::locate(const Mat& srcImg, Rect& srcRoi)
 //    if (tmpRoi.tl().inside(boundRoi) && tmpRoi.br().inside(boundRoi)) {
     // camshift reports success as long as ROI is at most half the size
     // of the input image, and at least 1 20th the size of the input image.
-    if (tmpRoi.area() >= srcImgSize.area()/400 && tmpRoi.area() <= srcImgSize.area()/2) {
+    if (tmpRoi.area() >= srcImgSize.area()/1200 && tmpRoi.area() <= srcImgSize.area()/2) {
         int newTLx = tmpRoi.x < 0 ? 0 : tmpRoi.x;
         int newTLy = tmpRoi.y < 0 ? 0 : tmpRoi.y;
         int newBRx = tmpRoi.br().x > srcImg.cols ? srcImg.cols : tmpRoi.br().x;
