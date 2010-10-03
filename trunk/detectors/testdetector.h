781b6d0495727095267cc9cc685e8a407992f562
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-10-03 16:41:20 +0000

    Added sandboxing class TestDetector.

---
+++ trunk/detectors/testdetector.h
@@ -0,0 +1,18 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <cv.h>
+#include "basedetector.h"
+
+class TestDetector : public BaseDetector
+{
+public:
+    TestDetector();
+
+    bool locate(const cv::Mat& srcImg, cv::Rect& srcRoi);
+
+private:
+    cv::Mat testImg;
+};
+
+#endif // TEST_H
