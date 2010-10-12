81ceda037f37989f34a30ed5b749752ac05d3930
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-10-12 17:55:57 +0000

    Summary: >> store.dispImg visualisations now render over the full 640x480    viewport if it is a scene visualition, or only over the relavant    region in the original scene image if store.dispImg is a face    or eyes visualation. Note that unlike the runtime filters, this    method of visualising filter effects does NOT modify the original    capture image - merely paints over it.
    >> store.dispImg is now used to point to visualisation images only.
       If uninitialised, it does no over-painting. sceneImg is always
       rendered now.
    
    Old OpenGL code remains in tact for now, albeit commented out.
    
    OpenGL:
    >> Fundamental change is in coupling OpenGL to the Model's store.
       Simplifies communication between the View and Model, since it
       can now directly ask the model (via store) for information when
       Control directs it to (via updateGL()). This is closer to the
       desired MVC model.
       Our original model needed Control to act as middle-man in passing
       Model's information over to View.

--- trunk/ui/glview.h
+++ trunk/ui/glview.h
@@ -4,34 +4,43 @@
 #include <cv.h>
 #include <qgl.h>
 
+class Store;
+
 class GLView : public QGLWidget
 {
     Q_OBJECT
 public:
-    GLView(std::vector<bool*> ivr, QWidget *parent = 0);
+    GLView(Store* st, QWidget* parent = 0);
+//    GLView(std::vector<bool*> ivr, QWidget *parent = 0);
     ~GLView();
 
     QSize minimumSizeHint() const;
     QSize sizeHint() const;
-    void loadGLTextures(const cv::Mat&);
-    void drawROIs(QRect* ROI);
-    void setFaceROI(int x, int y, int w, int h);
-    void setEyesROI(int x, int y, int w, int h);
+    void loadGLTextures();
+//    void loadGLTextures(const cv::Mat&);
+    void drawRois();
+//    void drawROIs(QRect* ROI);
+//    void setFaceROI(int x, int y, int w, int h);
+//    void setEyesROI(int x, int y, int w, int h);
 
 protected:
     void initializeGL();
     void paintGL();
     void resizeGL(int w, int h);
+
+    void drawRoi(int, int, int, int);
+
     void CheckGLError(const char*);
     int isExtensionSupported(const char *extension);
 
 private:
-    void drawLine();
-    QRect* faceROI;
-    QRect* eyesROI;
+    Store* store;
+    cv::Mat* dispImg;
+//    QRect* faceROI;
+//    QRect* eyesROI;
     GLuint texture;
     int npotCapable;
-    std::vector<bool*> isValidRoi;
+//    std::vector<bool*> isValidRoi;
 
 };
 
