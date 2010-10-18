5a5fbe69282f9ff802ecd0e437425cf1a6d244bf
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-10-18 06:44:30 +0000

    Some missed files from previous commits.

--- trunk/store.cpp
+++ trunk/store.cpp
@@ -1,12 +1,26 @@
 #include "store.h"
 
 Store::Store() :
+    calibMode(false),
+    calibX(0), calibY(0),
     faceLocated(false),
     eyesLocated(false),
     gazeLocated(false)
 {
-    // Initialisation of immediate-use variables
+    // Initialise these variables to be safe
     sceneImg = cv::Mat(FRAME_WIDTH, FRAME_HEIGHT, CV_8UC3, cv::Scalar(0,0,0));
+    faceImg = sceneImg.clone();
+    eyesImg = sceneImg.clone();
+    gazeImg = sceneImg.clone();
+
+    sceneMsk = cv::Mat(FRAME_WIDTH, FRAME_HEIGHT, CV_8UC1, cv::Scalar(255));
+    faceMsk = sceneMsk.clone();
+    eyesMsk = sceneMsk.clone();
+
     faceRoi = cv::Rect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+    eyesRoi = faceRoi;
+    gazeRoi = faceRoi;
+
+    // Set overlay image to nothing
     dispImg = &ignore;
 }
