336eb5cf06c82aefae950695bd6d7fdc983d5aa3
justcme <justcme@a09ac5dc-a04a-0975-72ff-3257ca587d63>
2010-10-24 11:58:13 +0000

    Sobel kernel size parameter rounds down if even.

--- filters/harriscornerfilter.cpp
+++ filters/harriscornerfilter.cpp
@@ -46,8 +46,7 @@ void HarrisCornerFilter::_filter(const cv::Mat& src)
 
     // Find Canny edges
     cvtColor(src, grayChannel, CV_BGR2GRAY);
-    sobelKernelSize = sobelKernelSize % 2 ? sobelKernelSize++ : sobelKernelSize;
-    cornerHarris(grayChannel, harrisImgF, neighbourhoodSize, sobelKernelSize, freeParamK);
+    cornerHarris(grayChannel, harrisImgF, neighbourhoodSize, sobelKernelSize + sobelKernelSize%2 - 1, freeParamK);
 
     // Convert from 32FC1 to 8UC1
     harrisImgF.convertTo(harrisImg, CV_8UC1, 255, 0);
